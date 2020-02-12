53048b3ea6a18d5c1a85dab84fd80269a1300b1a
alyona <sagan.alona201408@gmail.com>
2020-02-12 23:39:12 +0200

    Added task18_chaining

--- homework/hw_Alena/s07e01-rj-method-chaining.js
+++ homework/hw_Alena/s07e01-rj-method-chaining.js
@@ -1,5 +1,4 @@
-const newReleases = [
-  {
+const newReleases = [{
     "id": 70111470,
     "title": "Die Hard",
     "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
@@ -13,7 +12,10 @@ const newReleases = [
     "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
     "rating": 5.0,
-    "bookmark": [{ id: 432534, time: 65876586 }]
+    "bookmark": [{
+      id: 432534,
+      time: 65876586
+    }]
   },
   {
     "id": 65432445,
@@ -29,21 +31,25 @@ const newReleases = [
     "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
     "rating": 5.0,
-    "bookmark": [{ id: 432534, time: 65876586 }]
+    "bookmark": [{
+      id: 432534,
+      time: 65876586
+    }]
   }
 ];
 
-const getGoodReleasesIds = function() {
-  // ------------ INSERT CODE HERE! -----------------------------------
-  // Используйте методы filter и map в цепочке (method chaining - запомните термин!) , чтобы получить id всех видео
-  // с рейтингом 5.0
+const getGoodReleasesIds = function () {
+  const newRelease = newReleases
+    .filter(function (film) {
+      return film.rating === 5;
+    })
+    .map(function (film) {
+      return film.id;
+    });
+  return newRelease;
+};
 
-  return newReleases // закончите это выражение
-  // ------------ INSERT CODE HERE! -----------------------------------
-  // Если не получается сделать за 15 минут прочтите эту статью (ENG)
-  // https://gomakethings.com/chaining-array-methods-in-vanilla-js/
-}
-
-// Ожидаемый вывод:
-// [ 654356453, 675465 ]
 console.log(getGoodReleasesIds());
+
+// [ 654356453, 675465 ]
+// https://gomakethings.com/chaining-array-methods-in-vanilla-js/
