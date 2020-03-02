b1b6dc07a43ae82ec2220828fe812639312278d9
Denis Efremov <dagamitheslayer@gmail.com>
2020-03-02 10:44:03 +0200

    hw12_02

--- homework/s12e02-hw-objects.js
+++ homework/s12e02-hw-objects.js
@@ -8,23 +8,35 @@
 // число в консоль и метод isNegative, который
 // возвращает true если число меньше ноля.
 
-function /**
-
-<ВАШ_КОД_ТУТ>
-
-*/
+function RandomNumber() {
+  min = -100;
+  max = 100;
+  this.number = Math.floor(Math.random() * (max - min + 1)) + min;
+  this.showNumber = function() {
+    console.log("Number === " + this.number);
+  }
+  this.isNegative = function() {
+    if(this.number < 0) {
+      return true;
+    }
+    return false;
+  }
+}
 
 // 2. Создайте объект с помощью оператора new (в новую константу
 // присвойте результат вызова вашей функции счерез оператор new)
-
+const num = new RandomNumber();
 
 // 3. Создайте константу с пустым массивом. Используя цикл for
 // создайте 10 таких же объектов и с помощью push добавьте в массив
-
+const arr = [];
+for (let i = 0; i < 10; i++) {
+  arr.push(new RandomNumber());
+}
 
 // 4. Теперь, когда наши 10 объектов находятся в массиве мы можем
 // с ними работать, также как работали с объектами ранее.
 // Используйте метод массива filter и используя isNegative
 // из ваших объектов отберите отрицательные числа.
 // Используя forEach и метод объекта showNumber отобразите числа в консоль.
-
+arr.filter(obj => obj.isNegative()).forEach(element => element.showNumber());
