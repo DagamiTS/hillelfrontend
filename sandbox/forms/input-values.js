ddff519b7c954349012ced365f9c9e102fb3995e
Denis Efremov <dagamitheslayer@gmail.com>
2020-03-10 08:12:41 +0200

    hw with input has been done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,3 +1,8 @@
 function buttonClick() {
-
+  // const width = document.forms.cardForm.elements.gridWidth.value;
+  // const height = document.forms.cardForm.elements.gridHeight.value;
+  const width = document.querySelector("input[name=gridWidth]").value;
+  const height = document.querySelector("input[name=gridHeight]").value;
+  const result = document.getElementById("result");
+  result.innerText = width * height;
 }
