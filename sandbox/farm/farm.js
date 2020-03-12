487eb999cda782959b4b7806f413d4f7a84f3240
Denis Efremov <dagamitheslayer@gmail.com>
2020-03-12 13:05:31 +0200

    hw 15 has been done

--- sandbox/farm/farm.js
+++ sandbox/farm/farm.js
@@ -80,6 +80,52 @@ function Cow(x, y) {
 
 Cow.prototype = animal;
 
+function Pig(x, y) {
+  this._x = x;
+  this._y = y;
+  this.pixels = [
+    { x: -2 , y: -3 },
+    { x: -2 , y: -2 },
+    { x: -2 , y: -1 },
+    { x: 4 , y: -1 },
+    { x: 5 , y: -1 },
+    { x: 5 , y: -2 },
+    { x: -3 , y: 0 },
+    { x: -2 , y: 0 },
+    { x: -1 , y: 0 },
+    { x: 0 , y: 0 },
+    { x: 1 , y: 0 },
+    { x: 2 , y: 0 },
+    { x: 3 , y: 0 },
+    { x: -3 , y: -1 },
+    { x: -2 , y: -1 },
+    { x: -1 , y: -1 },
+    { x: -0 , y: -1 },
+    { x: 1 , y: -1 },
+    { x: 2 , y: -1 },
+    { x: 3 , y: -1 },
+    { x: -4 , y: 0 },
+    { x: -5 , y: 1 },
+    { x: -4 , y: 1 },
+    { x: -3 , y: 1 },
+    { x: -2 , y: 1 },
+    { x: -1 , y: 1 },
+    { x: -0 , y: 1 },
+    { x: 1 , y: 1 },
+    { x: 2 , y: 1 },
+    { x: 3 , y: 1 },
+    { x: -2 , y: 2 },
+    { x: -2 , y: 3 },
+    { x: 3 , y: 2 },
+    { x: 3 , y: 3 },
+  ];
+  this.move = function() {
+    this._x = this._x + (Math.random() - 0.5) * 3;
+    this._y = this._y + (Math.random() - 0.5) * 3;
+  }
+}
+
+Pig.prototype = animal;
 
 /** @type Animal[] */
 let animals = [];
@@ -96,6 +142,9 @@ function main() {
   }
   for (let i = 0; i < 5; i++) {
     animals.push(new Cow(Math.random() * canvas.width, Math.random() * canvas.height));
+  }
+  for (let i = 0; i < 10; i++) {
+    animals.push(new Pig(Math.random() * canvas.width, Math.random() * canvas.height));
   }
 }
 
--- sandbox/forms/checkerboard.js
+++ sandbox/forms/checkerboard.js
@@ -1,5 +1,54 @@
 function main() {
-
+  createCheckerBoard();
 }
 
-window.addEventListener(/** ВАШ КОД ВЕЗДЕ =) */);
+function createCheckerBoard() {
+  function getCell(x, y) {
+    const id = `x${x}y${y}`;
+    return document.getElementById(id);
+  }
+  
+  function createCell(x, y) {
+    const cell = document.createElement('div');
+    cell.id = `x${x}y${y}`;
+    cell.dataset.x = x;
+    cell.dataset.y = y;
+  
+    cell.style.left = `${x * CELL_SIZE}px`;
+    cell.style.top = `${y * CELL_SIZE}px`;
+    cell.style.width = `${CELL_SIZE}px`;
+    cell.style.height = `${CELL_SIZE}px`;
+    cell.style.position = "absolute";
+  
+    if (!((y % 2 !== 0 && x % 2 !== 0) || (y % 2 === 0 && x % 2 === 0))) {
+      cell.style.backgroundColor = "black";
+    }
+  
+    return cell;
+  }
+
+  const CELL_SIZE = 40;
+  const FIELD_SIZE = 8;
+  const BORDER_WIDTH = 5;
+
+  const board = document.getElementById("board");
+  board.style.width = `${CELL_SIZE * FIELD_SIZE + BORDER_WIDTH * 2}px`;
+  board.style.height = `${CELL_SIZE * FIELD_SIZE + BORDER_WIDTH * 2}px`;
+  board.style.position = "relative";
+  board.style.border = `${BORDER_WIDTH}px solid darkSlateGrey`;
+  
+  for (let x = 0; x < FIELD_SIZE; x++) {
+    for (let y = 0; y < FIELD_SIZE; y++) {
+      let cell = getCell(x, y);
+      if (!cell) {
+        cell = createCell(x, y);
+        board.appendChild(cell);
+      }
+      cell.className = '';
+    }
+  }
+}
+
+
+
+window.addEventListener("load",main);
