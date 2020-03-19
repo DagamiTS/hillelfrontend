8a3513fc671c28ff12c2a9fcc6d36ae5d28376a4
Denis Efremov <dagamitheslayer@gmail.com>
2020-03-19 13:10:46 +0200

    hw38 has been done

--- sandbox/traffic-light/traffic-light.js
+++ sandbox/traffic-light/traffic-light.js
@@ -1,23 +1,55 @@
-/**
- * Светофоры находятся в полном беспорядке.
- * Ваша здача починить их и заставить работать как надо с помощью
- * setInterval и setTimeout
- *
- * Для включения и выключения лампочек достаточно добавить или убрать
- * класс red, yellow или green у нужного светофора.
- * С помощью текста внутри елементов с классом "bulb" можно отображать
- * оставшееся количество секунд.
- *
- * Сейчас классы и значения установлены только для демонстрации возможностей.
- * Все значения можно очистить перед реализацией.
- *
- * Реализуйте переключение светофора настолько детализировано, насколько у вас получится.
- *
- * 0. Если необходимо, добавьте id или классы светофорам
- * 1. Начните с установки только красного и зеленого
- * 2. Добавьте желтый между зеленым и красным
- * 3. Добавьте жетлый вместе с красным перед зеленым
- * 4. Обратный отсчет - конечно, задание со звездочкой
- */
+function main() {
+  let leftTrafLights = document.querySelectorAll(".left");
+  let rightTrafLights = document.querySelectorAll(".light:not(.left)");
 
-/** YOUR CODE HERE */
+  leftTrafLights.forEach( el => {
+    el.classList.remove("red", "green", "yellow");
+  });
+  rightTrafLights.forEach( el => {
+    el.classList.remove("red", "green", "yellow");
+  });
+
+  function leftGreen() {
+    leftTrafLights.forEach( el => {
+      el.classList.remove("red", "yellow");
+      el.classList.add("green");
+    });
+    rightTrafLights.forEach( el => {
+      el.classList.remove("green", "yellow");
+      el.classList.add("red");
+    });
+  }
+
+  function rightGreen() {
+    rightTrafLights.forEach( el => {
+      el.classList.remove("red", "yellow");
+      el.classList.add("green");
+    });
+    leftTrafLights.forEach( el => {
+      el.classList.remove("green", "yellow");
+      el.classList.add("red");
+    });
+  }
+  
+  function yellow() {
+    rightTrafLights.forEach( el => {
+      el.classList.add("yellow");
+    });
+    leftTrafLights.forEach( el => {
+      el.classList.add("yellow");
+    });
+  }
+
+  function step() {
+    let timerLeftGreen = setTimeout(leftGreen, 0);
+    let timerYellow = setTimeout(yellow, 7000);
+    let timerRightGreen = setTimeout( rightGreen, 10000);
+    timerYellow = setTimeout(yellow, 17000);
+  }
+
+  step();
+
+  let intervalId = setInterval(step, 20000);
+}
+
+window.addEventListener("load", main);
